7d315cd97ef41aac0fb2dd94c9f38b72b0ebd5bb
amir.zaid <amir.zaid@silentmode.my>
2025-01-23 15:56:43 +0800

    fix(account): owners belongs to users

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/amirrhkm/bank.be/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 func (server *Server) createAccount(ctx *gin.Context) {
@@ -23,6 +24,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation", "foreign_key_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
